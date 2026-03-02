9790ef31b0d5a08369878052ee96745a562f8cb3
ssk.srikanth <ssk.srikanth@gmail.com>
2026-03-02 10:54:04 +0000

    made changes in package

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
@@ -11,10 +10,13 @@ const app = express();
 app.use(cors({ origin: true }));
 
 // Import and use your routes
-app.use("/api/users", require("./routes/users"));
-app.use("/api/services", require("./routes/services"));
-app.use("/api/bookings", require("./routes/bookings"));
+const usersRoutes = require("./routes/users");
+const servicesRoutes = require("./routes/services");
+const bookingsRoutes = require("./routes/bookings");
+
+app.use("/api/users", usersRoutes);
+app.use("/api/services", servicesRoutes);
+app.use("/api/bookings", bookingsRoutes);
 
 // Expose Express app as a Cloud Function
 exports.api = functions.https.onRequest(app);
-
